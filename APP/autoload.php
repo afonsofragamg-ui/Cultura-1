26f6f409723989ab163f5823fcd188ab9cf1c44a
Afonso Henrique Fraga <ahfsouzaengenharia@gmail.com>
2025-11-19 04:18:10 -0300

    a

--- APP/autoload.php
+++ APP/autoload.php
@@ -7,8 +7,8 @@ spl_autoload_register (function($nome_da_classe)
  $arquivo = BASE_DIR . "/" . $nome_da_classe . ".php";
  if (file_exists($arquivo))
     include $arquivo;
-//else
-    //throw new Exception("arquivo não existe");
+else
+    throw new Exception("arquivo não existe");
 
 }
 );
