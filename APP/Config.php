def653a9d6b7104eb0a03f9d21e3325d948301b4
afonsofragamg-ui <afonsofraga.mg@gmail.com>
2026-01-10 16:35:36 -0300

    q

--- APP/Config.php
+++ APP/Config.php
@@ -4,8 +4,8 @@
 // 3º Acesso ao Banco de Dados
 
 echo __FILE__;
-//define ('BASE_DIR',dirname(__FILE__,2));
-//define ('VIEWS',BASE_DIR .'/View');
+define ('BASE_DIR',dirname(__FILE__,2));
+define ('VIEWS',BASE_DIR .'/View');
 
 // isso aqui não é utilizado mas será aprendido mais adiante
 $_ENV ['db']['host'] = "localhost:3306";
--- APP/index.php
+++ APP/index.php
@@ -1,5 +1,5 @@
 <?php
 
 include "config.php";
-//include "autoload.php";
-//include "routes.php";
+include "autoload.php";
+include "routes.php";
